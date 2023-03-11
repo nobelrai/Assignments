a1a2e2a21161b6fc0b644ce802d7785005ffe772
nobelrai <mailtonobelrai@gmail.com>
2023-03-11 18:05:28 +0545

    introducing functions

--- anagram.py
+++ anagram.py
@@ -1,8 +1,21 @@
-a = input("Enter a word = ")
-b = input("Enter another word = ")
-c = sorted(a)
-d = sorted(b)
-if c == d:
-    print("Its anagram")
-else:
-    print("It's not an anagram")
+def anagram(first_word, second_word):
+    if sorted(first_word) == sorted(second_word):
+        return True
+    else:
+        return False
+
+
+def capitalize_anagram(first_word, second_word):
+    result = anagram(first_word, second_word)
+    if result is True:
+        return first_word.upper(), second_word.upper()
+    else:
+        return first_word, second_word
+
+
+a ="oow"
+b ="woo"
+c = "hello"
+d = "ehlol"
+print(anagram(a, b))
+print(capitalize_anagram(c, d))
